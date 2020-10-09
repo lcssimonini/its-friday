fe13252c92c368c7ff5cbaa3dc159cc98b173afd
Lucas Simonini <lucas.simonini@ilinksolutions.com.br>
2020-10-08 22:22:02 -0300

    roda na sexta

--- pages/index.js
+++ pages/index.js
@@ -23,16 +23,10 @@ export default function Home() {
   )
 }
 
-// function isFriday() {
-//   if(new Date().getDay() == 4) {
-//     return <audio autoplay id="my_audio" src="musica.mp3" loop="loop"></audio>
-//   }
-// }
-
 function getDayMessage() {
   var days = ["No", "Still no", "Not yet", "It is close", "I can smell it", "Yes, it is!", "It is gone."];
   var d = new Date().getDay();
-  if(d == 4) {
+  if(d == 5) {
     document.getElementById("my_audio").play();
   }
   return days[d];
